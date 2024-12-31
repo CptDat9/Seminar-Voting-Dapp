0d2209b0c7a4d1cbc03ce0414643c86fe58a99bb
CptDat9 <dat.dxc031205@gmail.com>
2025-01-01 00:50:43 +0700

    backend

---
+++ backend/hardhat.config.cjs
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@openzeppelin/hardhat-upgrades");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    bscTestnet: {
+      url: process.env.BSC_TESTNET,
+      accounts: [process.env.OWNER_PRIV_KEY],
+    },
+  },
+};
